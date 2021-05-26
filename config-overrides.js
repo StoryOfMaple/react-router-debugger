c9b6468434cb6dce379fa8ec985f4ee53f69dda0
张思彧 <1442987094@qq.com>
2021-05-27 01:50:09 +0800

    go

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ module.exports = function override(config, env) {
   config.resolve.alias = {
     'react-router': path.resolve(__dirname, 'src/react-router/index'),
     'react-router-dom': path.resolve(__dirname, 'src/react-router-dom/index')
-  }
+  };
+  config.devtool = 'source-map';
   return config;
 }
--- src/react-router-dom/BrowserRouter.js
+++ src/react-router-dom/BrowserRouter.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Router } from "react-router";
 import { createBrowserHistory as createHistory } from "history";
+// eslint-disable-next-line
 import PropTypes from "prop-types";
+// eslint-disable-next-line
 import warning from "tiny-warning";
 
 /**
@@ -11,26 +13,9 @@ class BrowserRouter extends React.Component {
   history = createHistory(this.props);
 
   render() {
+    debugger
     return <Router history={this.history} children={this.props.children} />;
   }
-}
-
-if (false) {
-  BrowserRouter.propTypes = {
-    basename: PropTypes.string,
-    children: PropTypes.node,
-    forceRefresh: PropTypes.bool,
-    getUserConfirmation: PropTypes.func,
-    keyLength: PropTypes.number
-  };
-
-  BrowserRouter.prototype.componentDidMount = function() {
-    warning(
-      !this.props.history,
-      "<BrowserRouter> ignores the history prop. To use a custom history, " +
-        "use `import { Router }` instead of `import { BrowserRouter as Router }`."
-    );
-  };
 }
 
 export default BrowserRouter;
